2f188cf794b90dc860baeb633c6f33578755941c
Michael Higgins <mdhiggins23@icloud.com>
2021-03-07 18:23:58 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from resources.readsettings import ReadSettings
-from autoprocess import sonarr
+from autoprocess import sonarr, radarr
 from resources.log import getLogger
 import sys
 
@@ -7,4 +7,5 @@ log = getLogger("Test")
 settings = ReadSettings()
 log.info("Initiating Test")
 log.debug(sys.argv[1])
-sonarr.processEpisode(sys.argv[1], settings, importMode="Copy", logger=log)
+# sonarr.processEpisode(sys.argv[1], settings, importMode="Copy", logger=log)
+radarr.processMovie(sys.argv[1], settings, importMode="Copy", logger=log)
